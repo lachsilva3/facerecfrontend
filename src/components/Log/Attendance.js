5c02ae3758b271b955f6fded5deeae1adeee69ee
Lachlan Da Silva <105099220+lachsilva3@users.noreply.github.com>
2023-03-03 13:24:54 +0530

    attendance log added

--- src/components/Log/Attendance.js
+++ src/components/Log/Attendance.js
@@ -1,69 +1,66 @@
 import React from "react";
+import {useState,useEffect} from 'react';
+import ReactHTMLTableToExcel from 'react-html-table-to-excel';
 import "./attendance_log.css";
-import { Table, Thead, Tbody, Tr, Th, Td } from 'react-super-responsive-table';
-import { Link, NavLink } from "react-router-dom";
 
 const Attendance=()=>{
+
+  const[result,setResult]= useState([]);
+ 
+  const getData = ()=>
+  {
+      fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(res => setResult( res));
+  }
+  
+  useEffect(() => {
+      getData();
+  },)
+
+
     return(
         <>
-          <div className="container3">
 
-<div class="box3">
- <br></br>
- <center><h1>Attendance Log</h1></center>
-  <br></br>
-  <Table>
-      <Thead>
-        <Tr>
-        <Th>Date</Th>
-          <Th>Student Name</Th>
-          <Th>Status</Th>
-        </Tr>
-      </Thead>
-      <Tbody>
-        
-        <Tr>
-        <Td></Td>
-        <Td></Td>
-        <Td></Td>
-        </Tr>
-        
-        <Tr>
-        <Td></Td>
-        <Td></Td>
-        <Td></Td>
-        </Tr>
-        
-        <Tr>
-        <Td></Td>
-        <Td></Td>
-        <Td></Td>
-        </Tr>
-        
-        <Tr>
-        <Td></Td>
-        <Td></Td>
-        <Td></Td>
-        </Tr>
-        
-        <Tr>
-        <Td></Td>
-        <Td></Td>
-        <Td></Td>
-        </Tr>
-        
-      </Tbody>
-    </Table>
+  <div className="container3">
+                <div className="row mt-3">
+                <center><h1>Attendance Log</h1></center>
+                {/*Table with generated data  */}
+                   <table className="table" id="table-to-xls">
+                    <thead className="thead-dark">
+                    <tr>
+                        <th>Date</th>
+                        <th>Student Name</th>
+                        <th>Status</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                   
+                         {result.map((res)=>
+                            <tr>
+                            <td>{res.name}</td>
+                            <td>{res.username}</td>
+                            <td>{res.email}</td>
+                            </tr>
+                          )}   
+                       
+                    </tbody>   
+                </table>
+{/* Button to download csv file */}
+                <center><ReactHTMLTableToExcel
+                    id="test-table-xls-button"
+                    className="download-table-xls-button btn3 btn-success mb-3"
+                    table="table-to-xls"
+                    filename="tablexls"
+                    sheet="tablexls"
+                    buttonText="Downlaod CSV File"/></center>
+             </div>
+            </div>
 
 
 
 
 
-
-
-
-</div>
-</div>
         
         
         
